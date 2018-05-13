26dc045f2d11c5d43308e59e7f29d8a7f8ef2eea
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2018-05-13 17:16:23 +0300

    Подготовка шаблона

---
+++ ZVV-Web-Lab-1/Controllers/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZVV_Web_Lab_1.Controllers
+{
+    public class MenuController : Controller
+    {
+        public string Main()
+        { return "<span>Главное меню</span>"; }
+
+        public string UserInfo()
+        {return "<span>Меню пользователя </span>"; }
+
+        public string Side()
+        { return "<span>Боковая панель</span>"; }
+
+        public string Map()
+        { return "<span>Карта сайта</span>"; }
+
+    }
+}
---
+++ ZVV-Web-Lab-1/Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZVV_Web_Lab_1.Models
+{
+    public class MenuItem
+    {
+    }
+}
