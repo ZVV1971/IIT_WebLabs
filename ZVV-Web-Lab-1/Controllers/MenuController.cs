5e00032c729c6984ee5e500a15d5af64d65ed927
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2018-05-13 18:29:52 +0300

    Controller Main has been changed

--- ZVV-Web-Lab-1/Controllers/MenuController.cs
+++ ZVV-Web-Lab-1/Controllers/MenuController.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZVV_Web_Lab_1.Models;
 
 namespace ZVV_Web_Lab_1.Controllers
 {
     public class MenuController : Controller
     {
-        public string Main()
-        { return "<span>Главное меню</span>"; }
+        List<MenuItem> items;
 
-        public string UserInfo()
-        {return "<span>Меню пользователя </span>"; }
+        public MenuController()
+        {
+            items = new List<MenuItem> {
+                new MenuItem { Name = "Домой", Controller = "Home", Action = "Index", Active = string.Empty },
+                new MenuItem { Name = "Каталог", Controller = "Product", Action = "List", Active = string.Empty },
+                new MenuItem { Name = "Администрирование", Controller = "Admin", Action = "Index", Active = string.Empty }
+            };
+        }
+
+        public PartialViewResult Main()
+        {
+            return PartialView(items);
+        }
+
+        public PartialViewResult UserInfo()
+        {
+            return PartialView();
+        }
 
         public string Side()
         { return "<span>Боковая панель</span>"; }
 
         public string Map()
         { return "<span>Карта сайта</span>"; }
-
     }
 }
--- ZVV-Web-Lab-1/Models/MenuItem.cs
+++ ZVV-Web-Lab-1/Models/MenuItem.cs
@@ -7,5 +7,9 @@ namespace ZVV_Web_Lab_1.Models
 {
     public class MenuItem
     {
+        public string Name { set; get; }        // Текст надписи
+        public string Controller { set; get; }  // Имя контроллера
+        public string Action { set; get; }      // Имя метода
+        public string Active { set; get; }      // Текущий пункт
     }
 }
